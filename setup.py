2a6278671184fdea7463730f134135a4d13bf645
luyuhuang <luyu_huang@foxmail.com>
2020-01-01 01:10:26 +0800

    add classifiers

--- setup.py
+++ setup.py
@@ -31,5 +31,18 @@ setup(
     },
     entry_points={
         "gui_scripts": ["DWords=DWords.__main__:main"]
-    }
+    },
+
+    classifiers=[
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+    ]
 )
